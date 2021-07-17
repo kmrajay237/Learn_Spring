ecc533824aac7ef8c72fa092b87169985bd508f7
Kumar Ajay <kumar.a.ajay@pwc.com>
2021-07-17 12:01:29 +0530

    Bean Life Cycle using interface

--- src/main/java/com/springcore/lifecycle/Test.java
+++ src/main/java/com/springcore/lifecycle/Test.java
@@ -7,10 +7,16 @@ public class Test {
 
 	public static void main(String[] args) {
 		AbstractApplicationContext context= new ClassPathXmlApplicationContext("com/springcore/lifecycle/config.xml");
-		Samosa s1 = (Samosa) context.getBean("s1");
-		System.out.println(s1);
+		
 //		Registering Shutdown hook
 		context.registerShutdownHook();
+		Samosa s1 = (Samosa) context.getBean("s1");
+		System.out.println(s1);
+
+		
+		System.out.println("************************");
+		Pepsi p1=(Pepsi) context.getBean("p1");
+		System.out.println(p1);
 	}
 
 }
